78337c0898d1fdba8ed633bbf4d887726ed37fcb
victoria.maron <victoria.maron@rga.com>
2023-05-09 11:53:51 -0300

    save changes

---
+++ ChallengeIntuit/Controllers/ClientController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChallengeIntuit.Controllers
+{
+    [ApiController]
+    [Route("client")]
+    public class ClientController : ControllerBase
+    {
+        //[HttpGet]
+        //[Route("get")]
+        
+    }
+}
